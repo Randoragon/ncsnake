87fb08f87edd6fa3734d3efaae23f4a37d93bdc6
Randoragon <randoragongamedev@gmail.com>
2020-08-02 12:06:17 +0200

    Add game loop, restructure code
    Moved function implementations from the header file to the source file.

--- ncsnake.c
+++ ncsnake.c
@@ -1,12 +1,42 @@
 #include <ncurses.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include "ncsnake.h"
 
+#define FPS 8
+
+// Implementations
+void die(char *msg, char *err)
+{
+    if (*err)
+        fprintf(stderr, "ncsnake: %s: %s\n", msg, err);
+    else
+        fprintf(stderr, "ncsnake: %s\n", msg);
+    exit(EXIT_FAILURE);
+}
+
+void updateDims()
+{
+    getmaxyx(stdscr, term_h, term_w);
+}
+
 int main(int argc, char **argv)
 {
+    // Initialization
     initscr();
+    updateDims();
 
+    // Enter game loop
+    running = 1;
+    while(running) {
+
+        refresh();
+        usleep(1000000 / FPS);
+    }
+
+    // Cleanup
     endwin();
+
     return EXIT_SUCCESS;
 }
--- ncsnake.h
+++ ncsnake.h
@@ -1,21 +1,12 @@
 #ifndef NCSNAKE_H
 #define NCSNAKE_H
 
-#include <stdio.h>
-#include <stdlib.h>
+// Variables
+int running;
+unsigned int term_h, term_w;
 
 // Forward declarations
 void die(char *msg, char *err);
-
-
-// Implementations
-void die(char *msg, char *err)
-{
-    if (*err)
-        fprintf(stderr, "ncsnake: %s: %s\n", msg, err);
-    else
-        fprintf(stderr, "ncsnake: %s\n", msg);
-    exit(EXIT_FAILURE);
-}
+void updateDims();
 
 #endif

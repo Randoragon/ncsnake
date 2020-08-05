495f2c1a43687489e7077c47919125c19af02bff
Randoragon <randoragongamedev@gmail.com>
2020-08-05 08:34:07 +0200

    Write functions for snake and player management
    To make it possible in the future to have multiple players at once, I've
    decided to create a new Player linked list which will hold every snake
    simultaneously. Every snake can have an independent direction and
    segments.
    
    All of these functions still need to be tested, which will happen as I
    try to implement game mechanics with them.

--- game.c
+++ game.c
@@ -5,6 +5,142 @@
 #define GAME_STAGE_MIN_W 4
 #define GAME_STAGE_MIN_H 4
 
+Snake *snakeCreate(SnakeCoords *coords, SnakeDir dir)
+{
+    int ishead = 1;
+    Snake *head = NULL, *new = NULL, *last;
+    for (SnakeCoords *c = coords; c; c++) {
+        last = new;
+        if (!(new = (Snake *)malloc(sizeof(Snake)))) {
+            if (head) {
+                snakeFree(head);
+            }
+            return NULL;
+        }
+        if (last) {
+            last->next = new;
+        }
+        new->x = c->x;
+        new->y = c->y;
+        new->dir = SNAKE_DIR_NONE;
+        new->ishead = ishead;
+        if (ishead) {
+            new->dir = dir;
+            head = new;
+            ishead = 0;
+        }
+    }
+    return head;
+}
+
+int snakeAppend(Snake *head, unsigned int y, unsigned int x)
+{
+    if (!head)
+        return 1;
+
+    Snake *new, *last;
+    if (!(new = (Snake *)malloc(sizeof(Snake)))) {
+        return 2;
+    }
+    new->x = x;
+    new->y = y;
+    new->dir = SNAKE_DIR_NONE;
+    new->ishead = 0;
+
+    for (last = head; last && last->next; last = last->next);
+    last->next = new;
+
+    return 0;
+}
+
+void snakeFree(Snake *head)
+{
+    Snake *seg, *tmp;
+    seg = head;
+    while (seg) {
+        tmp = seg;
+        seg = seg->next;
+        free(tmp);
+    }
+}
+
+Player *playersInit()
+{
+    Player *ret;
+    if (!(ret = (Player *)malloc(sizeof(Player)))) {
+        return NULL;
+    }
+    ret->head = NULL;
+    ret->next = NULL;
+    return ret;
+}
+
+int playersAdd(Player *players, Snake *head)
+{
+    if (!players)
+        return 1;
+
+    if (!players->head) {
+        players->head = head;
+    } else {
+        Player *last, *new;
+        for (last = players; last && last->next; last = last->next);
+        if (!(new = (Player *)malloc(sizeof(Player)))) {
+            return 1;
+        }
+        last->next = new;
+        new->head  = head;
+        new->next  = NULL;
+    }
+    return 0;
+}
+
+Player *playersRemove(Player *players, Player *player)
+{
+    if (players && player) {
+        if (player == players) {
+            players = player->next;
+        } else {
+            Player *prev;
+            for (prev = players; prev && prev->next != player; prev = prev->next);
+            if (prev) {
+                prev->next = player->next;
+            }
+        }
+        snakeFree(player->head);
+    }
+    return players;
+}
+
+void playersFree(Player *players)
+{
+    Player *p, *tmp;
+    p = players;
+    while (p) {
+        if (p->head) {
+            snakeFree(p->head);
+        }
+        tmp = p;
+        p = p->next;
+        free(tmp);
+    }
+}
+
+int playersSpawn(Player *players, GameStage *stage)
+{
+    for (Player *player = players; player; player = player->next) {
+        int ishead = 1;
+        for (Snake *seg = player->head; seg; seg = seg->next) {
+            if (seg->y >= stage->h || seg->x >= stage->w) {
+                return 1;
+            }
+            stage->tile[seg->y][seg->x] = (ishead ? GAME_TILE_SNAKE_HEAD : GAME_TILE_SNAKE_BODY);
+            ishead = 0;
+        }
+    }
+    return 0;
+}
+
 int gameStageCreate(GameStage *stage, unsigned int h, unsigned int w)
 {
     if (h < GAME_STAGE_MIN_H || w < GAME_STAGE_MIN_W) {
--- game.h
+++ game.h
@@ -1,18 +1,15 @@
 #ifndef GAME_H
 #define GAME_H
 
+// Constants
 typedef enum {
     GAME_TILE_EMPTY = 0,
     GAME_TILE_WALL,
-    GAME_TILE_SNAKE,
+    GAME_TILE_SNAKE_HEAD,
+    GAME_TILE_SNAKE_BODY,
     GAME_TILE_FOOD,
     GAME_TILE_UNKNOWN
 } GameTile;
-
-typedef struct {
-    GameTile **tile;
-    unsigned int w, h;
-} GameStage;
 
 typedef enum {
     SNAKE_STATE_ALIVE,
@@ -24,20 +21,50 @@ typedef enum {
     SNAKE_DIR_UP,
     SNAKE_DIR_LEFT,
     SNAKE_DIR_DOWN,
-    SNAKE_DIR_RIGHT
+    SNAKE_DIR_RIGHT,
+    SNAKE_DIR_NONE
 } SnakeDir;
+
+// Structs
+typedef struct GameStage GameStage;
+typedef struct Snake Snake;
+typedef struct SnakeCoords SnakeCoords;
+typedef struct Player Player;
+
+typedef struct GameStage {
+    GameTile **tile;
+    unsigned int w, h;
+} GameStage;
 
 typedef struct Snake {
     unsigned int x, y;
     char *ch;
     int ishead;
-    struct Snake *next;
+    SnakeDir dir;
+    Snake *next;
 } Snake;
 
-SnakeState snakeMove(Snake *head, SnakeDir dir);
-int gameStageCreate(GameStage *stage, unsigned int h, unsigned int w);
+typedef struct SnakeCoords {
+    unsigned int x, y;
+} SnakeCoords;
+
+typedef struct Player {
+    Snake *head;
+    Player *next;
+} Player;
+
+// Functions
+Snake *snakeCreate(SnakeCoords *coords, SnakeDir dir);
+int snakeAppend(Snake *head, unsigned int y, unsigned int x);
+void snakeFree(Snake *head);
+SnakeState snakeMove(Snake *head);
+Player *playersInit();
+int  playersAdd(Player *players, Snake *head);
+Player *playersRemove(Player *players, Player *player);
+void playersFree(Player *players);
+int  playersSpawn(Player *players, GameStage *stage);
+int  gameStageCreate(GameStage *stage, unsigned int h, unsigned int w);
 void gameStageFill(GameStage *stage, unsigned int h, unsigned int w, unsigned int y, unsigned int x, GameTile value);
 void gameStageSetDefault(GameStage *stage);
 
 #endif
-
